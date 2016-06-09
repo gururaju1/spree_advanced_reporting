da6ddb4b3eb526626829ded57d805780a3f80859
gururaju <gururaju57@gmail.com>
2016-06-09 15:27:41 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.draw do
+Spree::Core::Engine.routes.prepend do
   # Add your extension routes here
   
   match '/admin/reports/revenue' => 'admin/reports#revenue',  :via  => [:get, :post],
